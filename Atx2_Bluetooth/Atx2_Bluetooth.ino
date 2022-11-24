49cc0dcb7c8523e8aa508fd71c1f2e57a39b1f59
ArhonJay <118997864+ArhonJay@users.noreply.github.com>
2022-11-25 00:17:30 +0800

    Add files via upload

---
+++ Atx2_Bluetooth/Atx2_Bluetooth.ino
@@ -0,0 +1,16 @@
+#include <ATX2.h>	// ATX2 Board
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  OK();
+  Serial.begin(9600);
+  uart1_set_baud(9600);
+  uart1("Hello World");
+}
+
+void loop() { // run over and over
+  if (uart1_available()) {
+    char val = uart1_getkey();
+    Serial.print(val);
+  }
+}
